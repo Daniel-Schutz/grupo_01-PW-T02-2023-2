cecfa0dbd74e10690f2ba429be07074031fcaf0c
Daniel Schutz <109694403+Daniel-Schutz@users.noreply.github.com>
2023-11-25 14:39:18 -0400

    Update TelaPrincipal.js

--- src/components/TelaPrincipal.js
+++ src/components/TelaPrincipal.js
@@ -170,6 +170,7 @@ function TelaPrincipal() {
   };
 
   return (
+<<<<<<< HEAD
     <div>
       <header className='wrapePrincipalHeader'>
         <nav className='NavBarPrincipal'>
@@ -215,7 +216,7 @@ function TelaPrincipal() {
         <p>&copy; 2023 This or That - The Game. Todos os direitos reservados.</p>
       </footer>
     </div>
-
+=======
     
     <><header>
       <h1>Meu Cabeçalho</h1>
@@ -255,6 +256,7 @@ function TelaPrincipal() {
       </div>
       </>
 
+>>>>>>> 78257acdcb4eff78d158e85251fef454787af048
   );
 }
 
